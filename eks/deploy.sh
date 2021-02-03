9353da606fb3160e73d0439eb18b5f58deb04c2a
Diling Chen <chendil@cn.ibm.com>
2021-02-03 18:07:56 +0800

    add step to install kubectl

--- eks/deploy.sh
+++ eks/deploy.sh
@@ -9,38 +9,18 @@ echo
 echo "# Getting config of the cluster"
 echo
 aws eks update-kubeconfig --name my-cluster
-if [ $? -ne 0 ]; then
-    rc=$?
-    echo "# Failed to get config of cluster 'mycluster'."
-    exit $rc
-fi
 
 echo
 echo "# Checking if kubectl can work."
 echo
 kubectl get nodes
-if [ $? -ne 0 ]; then
-    rc=$?
-    echo "# Failed to get nodes info."
-    exit $rc
-fi
 
 echo
 echo "# Deploying the app to the cluster."
 echo
 kubectl apply -f ./services/myapp-go/deployment.yaml
-if [ $? -ne 0 ]; then
-    rc=$?
-    echo "# Failed to deploy the app to the cluster."
-    exit $rc
-fi
 
 echo
 echo "# Checking the app on the cluster."
 echo
 kubectl get pods -l app=myapp-go
-if [ $? -ne 0 ]; then
-    rc=$?
-    echo "# Failed to check the app on the cluster."
-    exit $rc
-fi
