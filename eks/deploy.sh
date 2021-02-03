cc6a883d03f77b8c68dd4d6a6825a074ef28c0bc
Diling Chen <chendil@cn.ibm.com>
2021-02-03 13:58:33 +0800

    config awscli with env variables

--- eks/deploy.sh
+++ eks/deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -e
 
-# AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY are set in environment variables
-AWS_DEFAULT_REGION="us-west-2"
-AWS_DEFAULT_OUTPUT="json"
+export AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}"
+export AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}"
+export AWS_DEFAULT_REGION="us-west-2"
+export AWS_DEFAULT_OUTPUT="json"
 
 echo
 echo "# Getting config of the cluster"
